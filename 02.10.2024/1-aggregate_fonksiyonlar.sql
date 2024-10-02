face2362df0f2b0b68778c7e4cd087f102478675
Recep <recepcengiz_96@outlook.com>
2024-10-02 17:44:56 +0300

    dersler eklendi

--- 02.10.2024/1-aggregate_fonksiyonlar.sql
+++ 02.10.2024/1-aggregate_fonksiyonlar.sql
@@ -1,5 +1,6 @@
 -- aggregate fonksiyonlar
 --mssql de ve oracle da grup fonks. olarak geçer. aggregate fonks. lar null değerleri atlayarak işlem yaparlar.
+-- Yalın sütunlarla çalışmazlar.
 
 -- avg: ortalama bulur. genelde select ve having ile kullanılır. null olmayan değerleri sayarak ortalamasını alır.
 --diyelim ki 12 tane değer geldi ve 4 tanesi null. 8 tanesini toplayıp 8 e böler.
@@ -32,10 +33,12 @@ from rental;
 select min(salary) from employees;
 select min(first_name) from employees; -- alfabetik olarak sıralar ve ilk değeri getirir.
 select min(hire_date) from employees; --tarihdeki min değer
+
 --max: verilen değerlerin max. bulur.
 select max(salary) from employees;
 select max(hire_date) from employees; --tarihdeki max değer
 select max(first_name) from employees; -- alfabetik olarak sıralar ve son değeri getirir.
+
 --count: verilen değerlerin sayısını bulur
 -- count(*) hiçbir şart olmadan bütün satır sayısını döner, 
 -- count(column): parametredeki column u null olmayan satırları sayar
@@ -43,3 +46,70 @@ select max(first_name) from employees; -- alfabetik olarak sıralar ve son değe
 select count(*) from actor;
 select count(phone_number) from employees;
 select count(distinct job_id) from employees;
+
+--alıştırmalar
+select 
+	min(length(first_name || last_name)) minumum,
+	max(length(first_name || last_name)) maximum,
+	round(avg(length(first_name || last_name)),2) ortalama
+from employees;
+
+--group by: verileri gruplamada kullanılır.
+--select içinde yazılan diğer yalın sütun isimleri group by içinde olmalıdır.
+-- group by içinde olanlar select içinde olmak zorunda değil. Column aliaslar group by içinde kullanılamazlar
+-- null olanlar da sorguya dahil edilir.
+
+-- birden çok sütuna göre gruplama. Önce ilk sıradakine göre sonra diğerlerine göre gruplar.
+select 
+	column1,
+	column2,
+	aggregate_function(column3)
+from
+	tablo_adi
+group by
+	column1,
+	column2
+
+-- tek columna göre gruplama
+select 
+	job_id,
+	count(*) nmbr,
+	min(salary),
+	max(salary)
+from employees
+group by job_id,
+order by 1
+
+--alıştırma
+select distinct rating from film;
+
+
+-- having: group by ile yapılan sorgularda şart yazmak için vardır. where ile çalışmıyor aggregate fonks.
+select 
+	column1,
+	aggregate_function(column2)
+from tablo_adi
+	group by column1
+	having group_by_exp
+	order by column1
+
+--alıştırma
+select customer_id, count(*) as adet
+from rental
+group by customer_id
+having count(*) > 35
+;
+
+
+select department_id,
+	count(*) as num_of_emps,
+	case 
+		when count(*) <=3 then 'Small Room'
+		when count(*) between 4 and 6 then 'Medium Room'
+		when count(*) >6 then 'Big Room'
+	end duration
+from employees
+group by 1
+order by 2;
+
+	
---
+++ 02.10.2024/2-join.sql
@@ -0,0 +1,29 @@
+-- tablo aliasları
+-- tablo_adi as alias_name
+
+-- join: tablo birleştirme işlemleri için kullanılır
+
+--inner join: iki veya daha fazla tablonun birleştirilmesi için kullanılır. Kesişimlerini getirir.
+select * 
+from basket_a a
+inner join
+basket_b b
+on a.fruit_a = b.fruit_b -- hangi tablolarda
+
+-- using: eğer join ettiğimiz tabloların sütun isimleri aynı ise using kullanırız. yani on yerine using kullanırız. Her iki tabloda
+-- da bu isim ortak ya. Bir güzel özelliği de normalde customer_id yi alias ile yapman lazımdı on ile kullanınca ama buna gerek
+-- kalmıyor.
+select customer_id,first_name 
+from customer
+inner join
+payment 
+using(customer_id)
+
+--alıştırma:
+
+
+--left join: soldaki tablonun tüm değerlerini getirir. sağdaki eşleşmiyorsa sağdaki değer null getirilir.
+--right join: sağdaki tablonun tüm değerlerini getirir. soldaki eşleşmiyorsa soldaki değer null getirilir.
+-- full join: İki tabloda da eşleşmeyen veriler olsa da getirilip solda ve sağda da null değerler olur.
+--self join: Bir tablonun kendisi ile birleştirilmesi
+-- cross join: İki tablonun eleman sayılarını çarparak bütün olasılıklarının hesaplandığı birleştirme
