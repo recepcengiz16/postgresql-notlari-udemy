a9b74578137d634e780487e11d0214500ec43e53
Recep <recepcengiz_96@outlook.com>
2024-10-03 14:55:39 +0300

    dersler eklendi

--- 02.10.2024/2-join.sql
+++ 02.10.2024/2-join.sql
@@ -23,7 +23,33 @@ using(customer_id)
 select first_name,last_name,department_name from employees inner join departments using(department_id);
 
 --left join: soldaki tablonun tüm değerlerini getirir. sağdaki eşleşmiyorsa sağdaki değer null getirilir.
+select * from film f left join inventory i on f.film_id = i.film_id 
+
 --right join: sağdaki tablonun tüm değerlerini getirir. soldaki eşleşmiyorsa soldaki değer null getirilir.
+select * from movies m right join movie_reviews mr using(movie_id)
+
 -- full join: İki tabloda da eşleşmeyen veriler olsa da getirilip solda ve sağda da null değerler olur.
+select * from x full outer join y using(id)
+
 --self join: Bir tablonun kendisi ile birleştirilmesi
+select * from table_a inner join table_a on predicate; -- self join kullanmıyoruz. inner, left, right join ile aynı 
+--tabloda kullanıyoruz.
+select * from employees e inner join employees e_ on e.manager_id = e_.employee_id;
+
+--alıştırma
+-- film tablosundaki aynı uzunluğa sahip olan filmleri getir. Aynı id leri tekrar getirmeye gerek yok.
+select * from film f1 inner join film f2 on f1.film_id <> f2.film_id and f1.length = f2.length
+
+--aynı şehirde yaşayan müşteriler
+
+
 -- cross join: İki tablonun eleman sayılarını çarparak bütün olasılıklarının hesaplandığı birleştirme
+select * from address cross join city; --dikkat et herhangi bir şart yok
+select * from address inner join city on true; -- bu da aynı şey
+select * from address,city -- bu da aynı şey
+
+--natural join:
+-- İki tabloyu birleştirirken bu iki tablodada ortak olan sütunu yazmadan otomatik kendi seçiyor.
+select * from t1 natural join t2; -- * kullanınca ortak sütunlar bir kez diğerleri normal bir şekilde gelir sorguda
+-- eğer ortak sütun yoksa cross join yapar. Eğer birden fazla ortak sütun varsa ve
+-- birebir aynı tipte değilse(mesela bir sütunu tarih ve zaman var diğerindeki sütun sadece tarih gibi) iki tabloda o zaman sorgu sonucu boş döner.
---
+++ 03.10.2024/1-combining_operators.sql
@@ -0,0 +1,25 @@
+-- combining operators(birleştirme operatörleri):
+-- birden fazla select cümlesini birleştirmede kullanılır. Veri tipleri uyumlu olmalıdır. Sütun sayıları aynı olmalıdır.  
+-- İki tane yazdıysan diğerinde de iki tane sütunu yazman lazım. Sıralamada da aynı veri tipleri olmalı
+
+-- union: İki verisetini birleştirir. Tekrar eden değerleri getirmez. İki sorgudaki aynı değerleri getirir.
+select * from table_a
+union
+select * from table_b
+
+--alıştırma
+select title,rating from film where rating='PG';
+union
+select title,rating from film where special_features::varchar like '%Trailers%';
+
+--union all: İki verisetini birleştirir. İki sorgudaki aynı değerleri getirir. Tekrar eden değerleri de getirir.
+select 'SQL1' as sq1,first_name,last_name from table_a
+union all
+select 'SQL2' as sq2, first_name,last_name from table_b -- 'SQL1' as sq1 diyerek de hangi sorgudan gelmiş 
+-- olduğunu anlamamızı sağlamak için böyle bir şey de yazabiliriz. Bunu sadece union all da yap yoksa union da bu sütunları da 
+-- farklı satır olarak algılayacağı için union all gibi olur o da
+
+--intersect: iki veya daha fazla olan sorgudaki kesişim kümesini verir. uniondan farkı union ortak satırları bir kez getiriyordu
+-- intersect bu ortak satırları getiriyor.
+
+--except:
